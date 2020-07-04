ffdb20fc450cdd9622c51f590d81dec28018e0cb
parkse <psmile1023@gmail.com>
2020-07-04 20:37:40 +0900

    User 적용, 환경별 설정 적용 후 커밋.

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const config = require('./config/key')
+
+const {User} = require('./models/User')
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://psmile23:Mdntdjdy1!0^^@boilerplate.lg1pr.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(()=> console.log('MongoDB Connected...'))
 .catch(err=> console.log(err))
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.post('/register', (req, res) =>{
+    const user = new User(req.body)
+    user.save((err, userInfo)=>{
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({success: true})
+    })
+})
+
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
--- models/User.js
+++ models/User.js
@@ -15,7 +15,7 @@ const userSchema = mongoose.Schema({
     },
     role:{
         type: String,
-        default: 'admin'
+        default: 'user'
     },
     image: String,
     token: {
@@ -26,5 +26,5 @@ const userSchema = mongoose.Schema({
     },
 })
 
-const User = mongoose.model('User', 'userSchema')
+const User = mongoose.model('User', userSchema)
 module.exports = {User}
